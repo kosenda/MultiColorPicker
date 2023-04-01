b8d87f08ee0b5b9fc034e3f9b035d5fc885d837a
ksnd <kkkk6311@yahoo.co.jp>
2023-04-01 12:56:59 +0900

    ktlint once

--- app/src/main/java/kosenda/makecolor/view/content/NewCategoryDialogContent.kt
+++ app/src/main/java/kosenda/makecolor/view/content/NewCategoryDialogContent.kt
@@ -12,9 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
