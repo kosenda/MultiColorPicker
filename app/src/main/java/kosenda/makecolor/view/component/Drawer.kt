b3ccaeeb90ec152f70ee364345268b11944efb12
ksnd <kkkk6311@yahoo.co.jp>
2023-04-02 12:37:50 +0900

    feature: add popupto in drawer

--- app/src/main/java/kosenda/makecolor/view/component/Drawer.kt
+++ app/src/main/java/kosenda/makecolor/view/component/Drawer.kt
@@ -69,6 +69,9 @@ fun Drawer(
                         else -> {
                             selectItemRoute = clickItem.route
                             navController.navigate(clickItem.route) {
+                                navController.graph.startDestinationRoute?.let { route ->
+                                    popUpTo(route) { saveState = false }
+                                }
                                 launchSingleTop = true
                                 restoreState = false
                             }
