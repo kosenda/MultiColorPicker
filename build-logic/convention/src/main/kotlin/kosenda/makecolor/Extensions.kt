2c504be8f829284640476f78651a301d9a64a87f
ksnd <kkkk6311@yahoo.co.jp>
2023-04-17 16:56:45 +0900

    feature: add android-library-plugin

---
+++ build-logic/convention/src/main/kotlin/kosenda/makecolor/Extensions.kt
@@ -0,0 +1,13 @@
+package kosenda.makecolor
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.plugins.ExtensionAware
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+/*
+ * @See GitHub-nowinandroid
+ * https://github.com/android/nowinandroid/blob/main/build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
+ */
+fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
+    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+}
--- core/util/build.gradle.kts
+++ core/util/build.gradle.kts
@@ -1,26 +1,11 @@
 plugins {
-    id(libs.plugins.android.library.get().pluginId)
-    id(libs.plugins.kotlin.android.get().pluginId)
+    id("multicolorpicker.android.library")
     id(libs.plugins.hilt.get().pluginId)
     id(libs.plugins.kotlin.kapt.get().pluginId)
 }
 
 android {
-    compileSdk = 33
     namespace = "kosenda.makecolor.core.util"
-    defaultConfig {
-        minSdk = 24
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-    kotlin {
-        jvmToolchain(17)
-    }
 }
 
 dependencies {
