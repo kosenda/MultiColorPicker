f0e9d783e398f0bf61b9a16d3568f0cc40447620
ksnd <kkkk6311@yahoo.co.jp>
2023-04-22 16:02:25 +0900

    chore: add test in RandomColorUtilityTest

--- core/util/src/test/java/kosenda/makecolor/core/util/RandomColorUtilityTest.kt
+++ core/util/src/test/java/kosenda/makecolor/core/util/RandomColorUtilityTest.kt
@@ -1,6 +1,7 @@
 package kosenda.makecolor.core.util
 
 import com.google.common.truth.Truth.assertThat
+import kosenda.makecolor.core.model.data.ColorData
 import kosenda.makecolor.core.model.data.RandomType
 import org.junit.Test
 
@@ -15,5 +16,10 @@ class RandomColorUtilityTest {
     fun outputRandomRGBColors_param1_size1() {
         assertThat(outputRandomRGBColors(randomType = RandomType.VIVID, size = 1).size)
             .isEqualTo(1)
+    }
+
+    @Test
+    fun randomColorData_output_isColorData() {
+        assertThat(randomColorData()).isInstanceOf(ColorData::class.java)
     }
 }
