7dab3928da6f16d74568c430dd6ca14dc51ecb2b
ksnd <kkkk6311@yahoo.co.jp>
2023-04-22 15:47:26 +0900

    feature: add mergeJacoco

--- build-logic/convention/src/main/kotlin/kosenda/makecolor/AndroidLibraryJacocoPugin.kt
+++ build-logic/convention/src/main/kotlin/kosenda/makecolor/AndroidLibraryJacocoPugin.kt
@@ -1,18 +1,7 @@
 package kosenda.makecolor
 
-import com.android.build.api.variant.ApplicationAndroidComponentsExtension
-import com.android.build.api.variant.LibraryAndroidComponentsExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.tasks.testing.Test
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.register
-import org.gradle.kotlin.dsl.withType
-import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
-import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
-import org.gradle.testing.jacoco.tasks.JacocoReport
 
 @Suppress("unused")
 class AndroidLibraryJacocoPlugin: Plugin<Project> {
