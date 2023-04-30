c872884e6212550ac6aab30365d474de949aace8
ksnd <kkkk6311@yahoo.co.jp>
2023-04-30 16:10:31 +0900

    bug-fix: add colorData in ColorDetailViewModelImpl init colorData

--- core/mock/src/main/java/kosenda/makecolor/core/mock/MockColorItem.kt
+++ core/mock/src/main/java/kosenda/makecolor/core/mock/MockColorItem.kt
@@ -3,11 +3,11 @@ package kosenda.makecolor.core.mock
 import kosenda.makecolor.core.model.data.ColorItem
 
 class MockColorItem {
-    val item = ColorItem(hex = "#FFFFFF", category = "test1", memo = "test1", id = 0)
+    val item = ColorItem(hex = "FFFFFF", category = "test1", memo = "test1", id = 0)
 
     val list = listOf(
         item,
-        ColorItem(hex = "#115599", category = "test2", memo = "test2", id = 1),
-        ColorItem(hex = "#AA00AA", category = "test3", memo = "test3", id = 2),
+        ColorItem(hex = "115599", category = "test2", memo = "test2", id = 1),
+        ColorItem(hex = "AA00AA", category = "test3", memo = "test3", id = 2),
     )
 }
